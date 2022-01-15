79f90e5211735d23b30b8d481db75cd275b5e35c
Kiran Raj R <kiranrajvjd@gmail.com>
2022-01-15 19:30:10 +0530

    --commit: Bubble Sort Algorithm

---
+++ sorting/BubbleSort/bubbleSorting.js
@@ -0,0 +1,22 @@
+// Author       : Kiran Raj R
+// Creation Date: 15/01/2022
+// Language Used: JavaScript
+// Description  : Bubble Sort Algorithm 
+
+
+function bubbleSort(array){
+    let length = array.length;
+    for(let i=0; i<length; i++){
+        for(let j=0; j<length-i; j++){
+            if(array[j]>array[j+1]){
+                let temp = array[j];
+                array[j] = array[j+1];
+                array[j+1] = temp;
+            }
+        }
+    }
+    console.log(array);
+}
+
+bubbleSort([34,12,5,78,9,1]);
+bubbleSort([1,12,5,78,9,10,34,0,-2]);
